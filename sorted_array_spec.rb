590e02f187673c3caa037004cd41acab342986b5
Alex Notov <anotov@gmail.com>
2014-01-13 14:34:27 -0800

    Add spec for map

--- sorted_array_spec.rb
+++ sorted_array_spec.rb
@@ -1,31 +1,57 @@
 require 'rspec'
 require './sorted_array.rb'
 
+shared_examples "return the array" do
+
+end
+
 describe SortedArray do
   before do
     @source = [2,3,4,7,9]
     @sorted_array = SortedArray.new @source
   end
 
-  describe :each do
-    context 
-    it 'should return the array' do
-      @sorted_array.each.should eq @source
-    end
+  describe "iterators" do
+    describe "that don't update the original array" do 
+      describe :each do
+        context 'when passed a block' do
+          specify do 
+            expect do |b| 
+              @sorted_array.each(&b) 
+            end.to yield_successive_args(2,3,4,7,9) 
+          end
+        end
 
-    context 'when passed a block' do
-      specify do 
-        expect do |b| 
-          @sorted_array.each(&b) 
-        end.to yield_successive_args(2,3,4,7,9) 
+        it 'should return the array' do
+          @sorted_array.each {|el| el }.should eq @source
+        end
+      end
+
+      describe :map do
+        it 'the original array should not be changed' do
+          original_array = @sorted_array.internal_arr
+          expect { @sorted_array.map {|el| el } }.to_not change { original_array }
+        end
+
+        specify do 
+          expect do |b| 
+            @sorted_array.map(&b) 
+          end.to yield_successive_args(2,3,4,7,9) 
+        end
+
+        it 'should apply the action in the block to each element and return the new array' do
+          @sorted_array.map { |el| el * 2 }.should == [4,6,8,14,18]
+        end
       end
     end
-  end
 
-  describe :map do
-  end
-
-  describe :map! do
+    describe "that update the original array" do
+      describe :map! do
+        it 'should return  array' do
+          pending
+        end
+      end
+    end
   end
 
   describe :find do
