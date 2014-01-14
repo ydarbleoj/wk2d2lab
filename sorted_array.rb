1e77dd04600b8be5c31a9d82df4eb835b5b11c98
Alex Notov <anotov@gmail.com>
2014-01-13 16:40:24 -0800

    Throw NotImplementedError for methods to be implemented

--- sorted_array.rb
+++ sorted_array.rb
@@ -12,17 +12,22 @@ class SortedArray
   end
 
   def each &block
+    raise NotImplementedError.new("You need to implement the each method!")
   end
 
   def map &block
+    raise NotImplementedError.new("You need to implement the map method!")
   end
 
   def map! &block
+    raise NotImplementedError.new("You need to implement the map! method!")
   end
 
   def find value
+    raise NotImplementedError.new("You need to implement the find method!")
   end
 
   def inject acc=nil, &block
+    raise NotImplementedError.new("You need to implement the inject method!")
   end
 end
